9721639f3eaebff43dc5c405f1cd3b9c0027ac93
KhaQuy <KhaQuy.khaquy1611@gmail.com>
2023-07-02 21:51:46 +0700

    update insert data scraper to models

---
+++ data/cate_brand.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        cate: 'Smartphone',
+        brand: ['Apple', 'Samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry']
+    },
+    {
+        cate: 'Tablet',
+        brand: ['Ipad', 'Samsung','Acer', 'Asus', 'Lenovo', 'Haier']
+    },
+    {
+        cate: 'Latop',
+        brand: ['Asus', 'Dell','Acer', 'Lenovo', 'HP', 'Macbook']
+    },
+    {
+        cate: 'Accessories',
+        brand: ['SmartWaches', 'Headphone','Bluethooth', 'Keyboard', 'Mouses', 'Cases']
+    },
+    {
+        cate: 'Television',
+        brand: ['Apple', 'Samsung', 'LG', 'Asus', 'Nokia', 'BlackBerry']
+    },
+    {
+        cate: 'Printer',
+        brand: ['Samsung', 'Acer','Asus', 'Lenovo', 'Haier']
+    },
+]
+
+module.exports = data;
