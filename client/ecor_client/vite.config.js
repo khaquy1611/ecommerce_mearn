80bc3a6892317cc669207491a7fd35b1bc8200b7
KhaQuy <KhaQuy.khaquy1611@gmail.com>
2023-07-20 17:46:43 +0700

    set up absolute path react

--- client/ecor_client/vite.config.js
+++ client/ecor_client/vite.config.js
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+const fileNames = ["src", "components", "assets", "axios", "hooks", "pages", "store", "ultils", "api"];
+const filePaths = fileNames.reduce((acc, cur) => ({
+  ...acc,
+  [cur]: `/${cur === "src" ? cur : "src/" + cur}`,
+}));
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace('/api', ''),
-      }
-    }
+        rewrite: (path) => path.replace("/api", ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      ...filePaths,
+    },
   },
   plugins: [react()],
-})
+});
